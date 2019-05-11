bb992ea84c35eeab37bce46cc323005aad7f4575
Aluno <aluno@imac10.local>
2019-05-11 15:34:46 +0100

    Initial Commit

---
+++ exemplo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exemplo2
+//
+//  Created by Aluno on 11/05/2019.
+//  Copyright © 2019 ipvc.estg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
